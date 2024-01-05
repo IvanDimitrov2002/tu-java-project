0a523edc008b6d539daed11107f5ce31937f5293
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2024-01-05 17:42:04 +0200

    feat: introduce Swagger and api descriptors

--- src/main/java/tu/social/project/controller/PostController.java
+++ src/main/java/tu/social/project/controller/PostController.java
@@ -5,6 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import tu.social.project.entity.CategoryEntity;
 import tu.social.project.payload.request.CreatePostRequest;
 import tu.social.project.payload.response.CreatePostResponse;
 import tu.social.project.payload.response.GetPostsResponse;
@@ -19,12 +26,23 @@ public class PostController {
     this.postService = postService;
   }
 
+  @Operation(summary = "Create a post with a title, content and author")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Created post", content = {
+          @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryEntity.class)) }),
+      @ApiResponse(responseCode = "400", description = "Invalid title, content or authorId supplied", content = @Content),
+  })
   @PostMapping
   public ResponseEntity<CreatePostResponse> createPost(@RequestBody CreatePostRequest request) {
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(postService.createPost(request));
   }
 
+  @Operation(summary = "Get a post by its author")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Found a post", content = {
+          @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryEntity.class)) }),
+  })
   @GetMapping("/author/{authorId}")
   public ResponseEntity<List<GetPostsResponse>> getPostsByAuthor(@PathVariable("authorId") String authorId) {
     return ResponseEntity.status(HttpStatus.OK)
--- src/main/java/tu/social/project/exception_handler/CategoryExceptionHandler.java
+++ src/main/java/tu/social/project/exception_handler/CategoryExceptionHandler.java
@@ -4,12 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import tu.social.project.exception.CategoryAlreadyExistsException;
 import tu.social.project.payload.response.ErrorResponse;
 
 @ControllerAdvice
 public class CategoryExceptionHandler {
     @ExceptionHandler(CategoryAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handle(CategoryAlreadyExistsException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
     }
