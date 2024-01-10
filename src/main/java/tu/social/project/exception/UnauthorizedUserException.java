6b48205fe3423a06dddbc0ce1ca7333f74c559fe
Maddie02 <madlensarkisian@gmail.com>
2024-01-10 14:10:42 +0200

    Added exception handling for unauthorized user exception

--- src/main/java/tu/social/project/exception/UnauthorizedUserException.java
+++ src/main/java/tu/social/project/exception/UnauthorizedUserException.java
@@ -1,4 +1,10 @@
 package tu.social.project.exception;
 
 public class UnauthorizedUserException extends RuntimeException {
+
+    private static final String MESSAGE = "You are not authorized to make this request!";
+    @Override
+    public String getMessage() {
+        return MESSAGE;
+    }
 }
--- src/main/java/tu/social/project/exception_handler/UserExceptionHandler.java
+++ src/main/java/tu/social/project/exception_handler/UserExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import tu.social.project.exception.UnauthorizedUserException;
 import tu.social.project.exception.UserAlreadyExistsException;
 import tu.social.project.payload.response.ErrorResponse;
 
@@ -15,6 +16,12 @@ public class UserExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handle(UserAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(UnauthorizedUserException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handle(UnauthorizedUserException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
     }
 
