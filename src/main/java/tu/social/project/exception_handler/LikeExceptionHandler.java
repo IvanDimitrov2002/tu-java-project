0c1ef468fc77324246c542284a4264ec2244a553
Maddie02 <madlensarkisian@gmail.com>
2024-01-10 14:11:21 +0200

    Added exception and handler for like that was not found

--- src/main/java/tu/social/project/exception_handler/LikeExceptionHandler.java
+++ src/main/java/tu/social/project/exception_handler/LikeExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import tu.social.project.exception.AlreadyLikedPostException;
+import tu.social.project.exception.LikeNotFoundException;
 import tu.social.project.payload.response.ErrorResponse;
 
 @ControllerAdvice
@@ -13,6 +14,12 @@ public class LikeExceptionHandler {
     @ExceptionHandler(AlreadyLikedPostException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handle(AlreadyLikedPostException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(LikeNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handle(LikeNotFoundException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
     }
 }
